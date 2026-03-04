bc953a309fa59c27f1469e974f5e380a260de012
Luis Alberto <luisalbertolaramartinez3c@gmail.com>
2026-03-04 13:56:14 +0100

    Creación de JwtService

---
+++ src/main/java/org/educa/homelyBackend/service/JwtService.java
@@ -0,0 +1,48 @@
+package org.educa.homelyBackend.service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Date;
+import java.util.Map;
+import java.util.UUID;
+
+@Service
+public class JwtService {
+
+    private final String secretKey;
+    private final String issuer;
+    private final String audience;
+    private final Clock clock;
+
+    public JwtService(
+            @Value("${jwt.secret.key}") String secretKey,
+            @Value("${jwt.issuer}") String issuer,
+            @Value("${jwt.audience}") String audience) {
+        this.secretKey = secretKey;
+        this.issuer = issuer;
+        this.audience = audience;
+        this.clock = Clock.systemUTC();
+    }
+
+    public String generateToken(String email, Map<String, Object> extraClaims) {
+        if (extraClaims == null) extraClaims = Map.of();
+        Instant now = clock.instant();
+        return Jwts.builder()
+                .issuer(issuer)
+                .audience().add(audience).and()
+                .id(UUID.randomUUID().toString())
+                .subject(email)
+                .claims(extraClaims)
+                .issuedAt(Date.from(now))
+                .expiration(Date.from(now.plus(Duration.ofDays(30))))
+                .signWith(Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8)))
+                .compact();
+    }
+}
