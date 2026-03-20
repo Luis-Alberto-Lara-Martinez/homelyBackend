7f6da2e33af4d51df5a3d6b0ee50a5dc78b0e3d2
Luis Alberto <luisalbertolaramartinez3c@gmail.com>
2026-03-20 13:00:16 +0100

    Creada la entity llamda PropertyResidenceEnergyCertificateDetails

---
+++ src/main/java/org/educa/homelyBackend/entity/PropertyResidenceEnergyCertificateDetails.java
@@ -0,0 +1,40 @@
+package org.educa.homelyBackend.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "property_residence_energy_certificate_details", indexes = {@Index(name = "uq_property_residence_energy_certificate_id_property",
+        columnList = "id_property",
+        unique = true)})
+public class PropertyResidenceEnergyCertificateDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "id_property", nullable = false)
+    private Properties idProperty;
+
+    @Column(name = "has_energy_certificate", nullable = false)
+    private Boolean hasEnergyCertificate;
+
+    @Column(name = "consumption_efficiency_scale", length = Integer.MAX_VALUE)
+    private String consumptionEfficiencyScale;
+
+    @Column(name = "consumption_efficiency_value")
+    private Integer consumptionEfficiencyValue;
+
+    @Column(name = "emissions_efficiency_scale", length = Integer.MAX_VALUE)
+    private String emissionsEfficiencyScale;
+
+    @Column(name = "emissions_efficiency_value")
+    private Integer emissionsEfficiencyValue;
+}
