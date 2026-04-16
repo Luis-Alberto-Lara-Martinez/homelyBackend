34150e47a8c3a36cb29f468c331e243ddc688ed4
Luis Alberto <luisalbertolaramartinez3c@gmail.com>
2026-04-16 13:22:54 +0200

    Simplificado y/o mejorado CloudinaryConfiguration

--- src/main/java/org/educa/homelyBackend/configuration/CloudinaryConfiguration.java
+++ src/main/java/org/educa/homelyBackend/configuration/CloudinaryConfiguration.java
@@ -1,10 +1,11 @@
 package org.educa.homelyBackend.configuration;
 
 import com.cloudinary.Cloudinary;
-import com.cloudinary.utils.ObjectUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
 
 @Configuration
 public class CloudinaryConfiguration {
@@ -14,9 +15,15 @@ public class CloudinaryConfiguration {
     private final String apiSecret;
 
     public CloudinaryConfiguration(
-            @Value("${cloudinary.cloud.name}") String cloudName,
-            @Value("${cloudinary.api.key}") String apiKey,
-            @Value("${cloudinary.api.secret}") String apiSecret) {
+            @Value("${cloudinary.cloud.name}")
+            String cloudName,
+
+            @Value("${cloudinary.api.key}")
+            String apiKey,
+
+            @Value("${cloudinary.api.secret}")
+            String apiSecret
+    ) {
         this.cloudName = cloudName;
         this.apiKey = apiKey;
         this.apiSecret = apiSecret;
@@ -24,7 +31,7 @@ public class CloudinaryConfiguration {
 
     @Bean
     public Cloudinary cloudinary() {
-        return new Cloudinary(ObjectUtils.asMap(
+        return new Cloudinary(Map.of(
                 "cloud_name", cloudName,
                 "api_key", apiKey,
                 "api_secret", apiSecret,
