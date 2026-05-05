bf83876f7b90315cf938dfdba557b60931055d00
Luis Alberto <luisalbertolaramartinez3c@gmail.com>
2026-05-05 16:21:21 +0200

    UpdateUserNameRequest borrado

--- src/main/java/org/educa/homelyBackend/dtos/requests/UpdateUserNameRequest.java
+++
@@ -1,9 +0,0 @@
-package org.educa.homelyBackend.dtos.requests;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record UpdateUserNameRequest(
-        @NotBlank(message = "El campo 'name' es requerido")
-        String name
-) {
-}
