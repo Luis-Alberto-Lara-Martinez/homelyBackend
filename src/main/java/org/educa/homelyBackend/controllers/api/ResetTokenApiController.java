1cab3242c5377e0f7a25e1fc2f37fc7b6ee43d04
Luis Alberto <luisalbertolaramartinez3c@gmail.com>
2026-05-18 09:06:51 +0200

    Arreglo de permisos en RestTokenApiController

--- src/main/java/org/educa/homelyBackend/controllers/api/ResetTokenApiController.java
+++ src/main/java/org/educa/homelyBackend/controllers/api/ResetTokenApiController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RequestMapping(ConfigurationRoutes.API)
 @RequiredArgsConstructor
 public class ResetTokenApiController {
 
